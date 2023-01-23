c10d7b865bfef19bdf1cd6506167f9a9575dcb1b
jeanoza <choikb916@gmail.com>
2023-01-23 21:20:50 +0100

    fix:push

--- frontend/components/navbar/pageList.tsx
+++ frontend/components/navbar/pageList.tsx
@@ -1,24 +1,38 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const elements = ["/", "/note"];
+//FIXME: use object and entry will be better
+//ex:
+const elements = { home: "/", note: "/note" }
+//const elements = ["/", "/note"];
 
 export function PageList() {
 	const router = useRouter();
 
 	return (
+		//<ul>
+		//	{elements.map((el) => {
+		//		return (
+		//			<li key={el}>
+		//				<Link href={el}>
+		//					<span className={`${router.pathname === el ? "active" : ""}`}>
+		//						{el === "/" ? "Home" : el[1].toUpperCase() + el.slice(2)}
+		//					</span>
+		//				</Link>
+		//			</li>
+		//		);
+		//	})}
+		//</ul>
 		<ul>
-			{elements.map((el) => {
-				return (
-					<li key={el}>
-						<Link href={el}>
-							<span className={`${router.pathname === el ? "active" : ""}`}>
-								{el === "/" ? "Home" : el[1].toUpperCase() + el.slice(2)}
-							</span>
-						</Link>
-					</li>
-				);
-			})}
+			{Object.entries(elements).map(el => (
+				<li key={el[0]}>
+					<Link href={el[1]}>
+						<span className={`${router.pathname === el[1] ? "active" : ""}`}>
+							{el[0][0].toUpperCase() + el[0].slice(1)}
+						</span>
+					</Link>
+				</li>
+			))}
 		</ul>
 	);
 }
