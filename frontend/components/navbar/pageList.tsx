043181020b7d53bc8ce50cbc44356ad1cf51931e
jeanoza <choikb916@gmail.com>
2023-02-15 09:55:05 +0100

    fix(frontend):42 logo implemented

--- frontend/components/navbar/pageList.tsx
+++ frontend/components/navbar/pageList.tsx
@@ -1,41 +1,40 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-const elements = {
-	home: "/",
-	//note: "/note",
-	chat: "/chat",
-	//settings: "/settings",
-};
-
 export function PageList() {
 	const router = useRouter();
 
 	return (
 		<ul className="d-flex center">
-			{Object.entries(elements).map((el) => {
-				const name = el[0];
-				const path = el[1];
-				return (
-					<li key={name}>
-						<Link href={path}>
-							<span
-								className={`${router.pathname === path ||
-									(router.pathname !== "/" && router.pathname.includes(name))
-									? "active"
-									: ""
-									}`}
-							>
-								{name}
-							</span>
-						</Link>
-					</li>
-				);
-			})}
+			<li>
+				<Link href="/">
+					<img src="/42_icon.svg" />
+				</Link>
+			</li>
+			<li>
+				<Link href="/chat">
+					<span className={(router.pathname.includes("chat")) ? "active" : ""} >
+						chat
+					</span>
+				</Link>
+			</li>
+			<li>
+				<Link href="#">
+					<span className={(router.pathname.includes("game")) ? "active" : ""} >
+						game
+					</span>
+				</Link>
+			</li>
 			<style jsx>{`
+				img{
+					width:2rem;
+					height:auto;
+				}
 				ul > li {
-					display: block;
 					padding: 1.2rem;
+				}
+				ul > li:first-child {
+					padding:0 1.2rem;
 				}
 				ul > li:hover {
 					color: white;
