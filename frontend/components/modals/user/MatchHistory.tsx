8d5c099f25a785df94a5c9585146a8cc25760daf
jeanoza <choikb916@gmail.com>
2023-03-06 10:16:50 +0100

    tweaks(frontend): match history in user modal

--- frontend/components/modals/user/MatchHistory.tsx
+++ frontend/components/modals/user/MatchHistory.tsx
@@ -25,22 +25,19 @@ export function MatchHistory({ userId }: { userId: number }) {
 					const isWinner = match.winner.id === userId ? true : false;
 					const _score = match.score;
 					const score = _score[0] + " : " + _score[1];
+					const vs = isWinner ? match.loser : match.winner;
 					return (
 						<li key={index} className="d-flex center justify-between my-2 mx-4">
-							<div className="d-flex column center">
-								<Avatar size="sm" url={match.winner.imageURL} />
-								<h4 className="text-overflow">{match.winner.name}</h4>
-							</div>
 							<h3>VS</h3>
 							<div className="d-flex column center">
-								<Avatar size="sm" url={match.loser.imageURL} />
-								<h4 className="text-overflow">{match.loser.name}</h4>
+								<Avatar size="sm" url={vs.imageURL} />
+								<h4 className="text-overflow">{vs.name}</h4>
 							</div>
 							<div className="d-flex column center">
 								<h3>{score}</h3>
 								{isWinner
-									? <span className="text won">Won</span>
-									: <span className="text lost">Lost</span>
+									? <h4 className="text won">Won</h4>
+									: <h4 className="text lost">Lost</h4>
 								}
 							</div>
 							<span className="date">
@@ -61,21 +58,17 @@ export function MatchHistory({ userId }: { userId: number }) {
 					overflow-y: auto;
 				}
 				.text-overflow {
-					width: 5rem;
+					width: 7rem;
 					text-align: center;
 				}
 				.text {
 					margin:auto;
-					font-weight:500;
 				}
 				.text.won {
 					color: var(--accent);
 				}
 				.text.lost {
 					color: red;
-				}
-				.date {
-					width:5rem;
 				}
 			`}</style>
 		</div>
