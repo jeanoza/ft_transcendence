f9e8b100befe2086cd3abda51238c77ed6f30298
Kyubong CHOI <kychoi@student.42.fr>
2023-03-09 14:55:27 +0100

    fix(matchModal): done

--- frontend/components/modals/matchModal.tsx
+++ frontend/components/modals/matchModal.tsx
@@ -1,23 +1,9 @@
-import axios from "axios";
 import { useUser } from "../../utils/hooks/swrHelper";
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { MatchHistory } from "./user/MatchHistory";
 
 export function MatchModal({ onClose }: { onClose: any }) {
 	const { user } = useUser();
-
-	useEffect(() => {
-		getMatch();
-		async function getMatch() {
-			axios.get("match").then(res => {
-				// this data is ordered by date(descendant)
-				// res.data[0] is the more recent match
-				// put this data in state with setState
-				// then do your self what you want!
-				console.log(res.data);
-			})
-		}
-	}, [])
-
 
 	function handleClose(e: any) {
 		if (e.target.classList.contains("modal-background")) onClose();
@@ -27,10 +13,9 @@ export function MatchModal({ onClose }: { onClose: any }) {
 	return (
 		<div className="modal-background" onClick={handleClose}>
 			<div className="modal-container">
+				<MatchHistory userId={user.id} />
 			</div>
-			<style jsx>{`
-			
-			`}</style>
+			<style jsx>{``}</style>
 		</div>
 	);
 }
