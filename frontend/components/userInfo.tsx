5bd8faca543bf0ccf1ab4a9a165116d6f0b90f31
Kyubong CHOI <kychoi@student.42.fr>
2023-03-27 12:06:18 +0200

    feat(zod): installed to protect submit action

--- frontend/components/userInfo.tsx
+++ frontend/components/userInfo.tsx
@@ -1,18 +1,25 @@
-import React, { useState } from "react"
-import { InputField } from "./inputField"
-import axios from "axios"
-import { useUser } from "../utils/hooks/swrHelper"
+import React, { useState } from "react";
+import { InputField } from "./inputField";
+import axios from "axios";
+import { useUser } from "../utils/hooks/swrHelper";
+import zod from "zod";
 
-export function UserInfo({ user, isEditModal }: { user: IUser, isEditModal?: boolean }) {
-	const [name, setName] = useState<string | any>(user.name)
+export function UserInfo({
+	user,
+	isEditModal,
+}: {
+	user: IUser;
+	isEditModal?: boolean;
+}) {
+	const [name, setName] = useState<string | any>(user.name);
 	const { revalid } = useUser();
 
 	async function onSubmitName(e: any) {
 		if (e.code === "Enter" && name.length) {
 			if (name === user.name) return;
 			try {
-				await axios.patch('user', { name: name.trim() })
-				window.alert('updated')
+				await axios.patch("user", { name: name.trim() });
+				window.alert("updated");
 				revalid();
 			} catch (e: any) {
 				window.alert(e?.response?.data.message);
@@ -20,35 +27,42 @@ export function UserInfo({ user, isEditModal }: { user: IUser, isEditModal?: boo
 		}
 	}
 
+	console.log(zod);
+
 	return (
 		<div className="user-info">
-			{isEditModal ?
-				<InputField type="text" name="name" state={name} setState={setName} onKeyUp={onSubmitName} />
-				: <h3>{user.name}</h3>
-			}
+			{isEditModal ? (
+				<InputField
+					type="text"
+					name="name"
+					state={name}
+					setState={setName}
+					onKeyUp={onSubmitName}
+				/>
+			) : (
+				<h3>{user.name}</h3>
+			)}
 			<div className="d-flex column gap">
 				<div className="d-flex justify-between">
 					<span>Email</span>
-					<span>
-						{user.email}
-					</span>
+					<span>{user.email}</span>
 				</div>
 				<div className="d-flex justify-between">
 					<span>Created at</span>
-					<span>{user.createdAt && new Date(user.createdAt).toLocaleDateString()}</span>
+					<span>
+						{user.createdAt && new Date(user.createdAt).toLocaleDateString()}
+					</span>
 				</div>
 				<div className="d-flex justify-between">
 					<span>Rank</span>
-					<span>
-						{user.rank}
-					</span>
+					<span>{user.rank}</span>
 				</div>
 			</div>
 			<style jsx>{`
 				h3 {
-					margin-bottom:1rem
+					margin-bottom: 1rem;
 				}
 			`}</style>
 		</div>
-	)
+	);
 }
